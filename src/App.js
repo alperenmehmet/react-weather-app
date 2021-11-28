63131817f803da7d742b4aa86902b4fb96ab0189
Mehmet ALPEREN <me.alperen@gmail.com>
2021-11-28 23:53:10 +0300

    Components created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { WeatherContainer } from './components/WeatherContainer';
+import { WeatherInfo } from './components/WeatherInfo';
+import { WeekContainer } from './components/WeekContainer';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='container'>
+      <WeatherContainer />
+      <WeatherInfo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/WeatherInfo.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export const WeatherInfo = () => {
+  return (
+    <div className='info-side'>
+      <div className='today-info-container'>
+        <div className='today-info'>
+          <div className='precipitation'>
+            {' '}
+            <span className='title'>PRECIPITATION</span>
+            <span className='value'>0 %</span>
+            <div className='clear'></div>
+          </div>
+          <div className='humidity'>
+            {' '}
+            <span className='title'>HUMIDITY</span>
+            <span className='value'>34 %</span>
+            <div className='clear'></div>
+          </div>
+          <div className='wind'>
+            {' '}
+            <span className='title'>WIND</span>
+            <span className='value'>0 km/h</span>
+            <div className='clear'></div>
+          </div>
+        </div>
+      </div>
+      <div className='week-container'>
+        <ul className='week-list'>
+          <li className='active'>
+            <i className='day-icon' data-feather='sun'></i>
+            <span className='day-name'>Tue</span>
+            <span className='day-temp'>29°C</span>
+          </li>
+          <li>
+            <i className='day-icon' data-feather='cloud'></i>
+            <span className='day-name'>Wed</span>
+            <span className='day-temp'>21°C</span>
+          </li>
+          <li>
+            <i className='day-icon' data-feather='cloud-snow'></i>
+            <span className='day-name'>Thu</span>
+            <span className='day-temp'>08°C</span>
+          </li>
+          <li>
+            <i className='day-icon' data-feather='cloud-rain'></i>
+            <span className='day-name'>Fry</span>
+            <span className='day-temp'>19°C</span>
+          </li>
+          <div className='clear'></div>
+        </ul>
+      </div>
+    </div>
+  );
+};
