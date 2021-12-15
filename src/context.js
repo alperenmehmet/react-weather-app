3d91a6866c2594e1e71af13d7edeb65fef017358
Mehmet ALPEREN <me.alperen@gmail.com>
2021-12-15 11:46:18 +0300

    Components finished.

--- src/context.js
+++ src/context.js
@@ -1,18 +1,35 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 
-export const AppContext = React.createContext();
+const AppContext = React.createContext();
 
-export const AppProvider = ({ children }) => {
-  const [city, setCity] = useState('');
-  const [data, setData] = useState([]);
+const AppProvider = ({ children }) => {
+  const [weather, setWeather] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  return (
-    <AppContext.Provider value={{ city, setCity, data, setData }}>
-      {children}
-    </AppContext.Provider>
-  );
+  const fetchWeather = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=london&APPID=${process.env.REACT_APP_API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+      if (data) {
+        setWeather(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchWeather();
+  }, [setWeather]);
+
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
+
+export { AppContext, AppProvider };
